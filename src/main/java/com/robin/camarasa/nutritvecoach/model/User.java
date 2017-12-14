c61ffbc79ae03864cb7aed6446e02701dc1a6392
robin.camarasa <robin.camarasa@etu.emse.fr>
2017-12-14 16:22:28 +0100

    test

--- src/main/java/com/robin/camarasa/nutritvecoach/model/User.java
+++ src/main/java/com/robin/camarasa/nutritvecoach/model/User.java
@@ -17,7 +17,7 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @ManyToOne
     private PhysicalData physicalData;
 
     @SuppressWarnings("unused")
