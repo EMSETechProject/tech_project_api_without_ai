6cfd956f904ad9fc6b051dde966f4fc01db66b28
robin.camarasa <robin.camarasa@etu.emse.fr>
2017-12-15 15:47:09 +0100

    test

--- src/main/java/com/robin/camarasa/nutritvecoach/web/Controller/WeightController.java
+++ src/main/java/com/robin/camarasa/nutritvecoach/web/Controller/WeightController.java
@@ -5,6 +5,7 @@ import com.robin.camarasa.nutritvecoach.dao.UserDao;
 import com.robin.camarasa.nutritvecoach.dao.WeightDao;
 import com.robin.camarasa.nutritvecoach.model.User;
 import com.robin.camarasa.nutritvecoach.model.Weight;
+import com.robin.camarasa.nutritvecoach.web.dto.UserDto;
 import com.robin.camarasa.nutritvecoach.web.dto.WeightDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -44,16 +45,20 @@ public class WeightController {
     }
 
     @GetMapping(value = "/findlastten/{id}")
-    public List<WeightDto> getLastTen(@PathVariable Long id) {
+    public List<UserDto> getLastTen(@PathVariable Long id) {
+        List<UserDto> userDtos = new ArrayList<>();
         List<Weight> weights = weightDao.findAll();
         List<Weight> weights1 = new ArrayList<>();
         for (Weight weight : weights) {
-            if(weight.getUser().getId() == id) {
+            /*if(weight.getUser().getId() == id) {
                 return weights.stream().map(WeightDto::new).collect(Collectors.toList());
                 //weights1.add(weight);
-            }
+            }*/
+            userDtos.add(new UserDto(weight.getUser()));
+
         }
         //List<Weight> weights2 = weights.subList(Math.max(0,weights1.size()-10),weights1.size()-1);
-        return weights1.stream().map(WeightDto::new).collect(Collectors.toList());
+        //return weights1.stream().map(WeightDto::new).collect(Collectors.toList());
+        return userDtos;
     }
 }
