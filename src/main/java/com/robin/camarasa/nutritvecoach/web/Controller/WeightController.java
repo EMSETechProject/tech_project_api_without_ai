ceb6f160938587147982ec4c433d29f28afd6b67
robin.camarasa <robin.camarasa@etu.emse.fr>
2017-12-15 15:36:38 +0100

    added /weights/findlasten10

--- src/main/java/com/robin/camarasa/nutritvecoach/web/Controller/WeightController.java
+++ src/main/java/com/robin/camarasa/nutritvecoach/web/Controller/WeightController.java
@@ -52,7 +52,7 @@ public class WeightController {
                 weights1.add(weight);
             }
         }
-        List<Weight> weights2 = weights1.subList(Math.max(0,weights1.size()-10),weights1.size()-1);
+        List<Weight> weights2 = weights.subList(Math.max(0,weights1.size()-10),weights1.size()-1);
         return weights2.stream().map(WeightDto::new).collect(Collectors.toList());
     }
 }
